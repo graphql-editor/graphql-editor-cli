1eb3185ac574627f3c1349afdf74e2da3ef8a6b2
aexol <aexol@aexol.com>
2020-12-20 19:58:28 +0100

    correct models generation

--- src/code/systems/mongoTs/core/models/index.ts
+++ src/code/systems/mongoTs/core/models/index.ts
@@ -7,21 +7,20 @@ export const getModel = async (modelsPath: string, field: ParserField) => {
   HandleTemplates.action({
     type: 'addIfNotExist',
     path: `${modelsPath}/${modelName}.ts`,
-    content: `
-import { ModelTypes} from '../../graphql-zeus';
+    content: `import { ModelTypes} from '../../graphql-zeus';
 // Create your model here as it is stored in MongoDB
 export type ${modelName} = ModelTypes['${field.name}'];`,
   });
   return modelName;
 };
 
-export const generateModelsFile = (collectionsPath: string, modelsPath: string, fields: ParserField[]) => {
+export const generateModelsFile = async (collectionsPath: string, modelsPath: string, fields: ParserField[]) => {
   const modelFields = fields.filter((f) => f.data.type === TypeDefinition.ObjectTypeDefinition);
   let importsContent = '';
   const importCollections = [];
   let modelsContent = [];
   for (const f of modelFields) {
-    const modelName = getModel(modelsPath, f);
+    const modelName = await getModel(modelsPath, f);
     const collectionName = getCollection(collectionsPath, f);
     importsContent += `import { ${modelName} } from './${modelName}'\n`;
     importCollections.push(collectionName);
