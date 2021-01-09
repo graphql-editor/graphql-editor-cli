bfc5d9798c90cde9c8a3e70aaf5bb24dafdca18f
aexol <aexol@aexol.com>
2021-01-10 00:22:46 +0100

    drastically improved operations of orm

--- src/code/systems/mongoTs/templates/orm/operations.ts
+++ src/code/systems/mongoTs/templates/orm/operations.ts
@@ -1,59 +1,37 @@
 export const operations = () => {
   return {
-    ts: `import { Db } from 'mongodb';
-import * as collections from './collections';
-import { Models } from './models';
-
-type PartialNull<T> = { [P in keyof T]?: T[P] | undefined | null };
-
-export const Orm = <T extends keyof Models>(db: Db, collection: T) => {
-    const c = collections[collection];
-    const create = (props: Partial<Models[T]>) => {
-      return db.collection(c).insertOne(props);
+    ts: `import { Db, FilterQuery, ObjectId, OptionalId, UpdateQuery } from 'mongodb';
+    import * as collections from './collections';
+    import { Models } from './models';
+    
+    type PartialNull<T> = { [P in keyof T]?: T[P] | undefined | null };
+    
+    export const Orm = <T extends keyof Models>(db: Db, collection: T) => {
+      type Model = Models[T] & { _id: ObjectId };
+      const c = collections[collection];
+      const create = db.collection<Model>(c).insertOne;
+      const update = db.collection<Model>(c).updateOne;
+      const one = db.collection<Model>(c).findOne;
+      const oneOrThrow = async (filter: FilterQuery<Model>) => {
+        const o = await db.collection<Model>(c).findOne(filter);
+        if (!o) {
+          throw new Error(\`Cannot find \${c} object for that params\`);
+        }
+        return o;
+      };
+      const list = (filter: FilterQuery<Model> = {}) => db.collection<Model>(c).find(filter).toArray();
+      const remove = db.collection<Model>(c).deleteOne;
+      const removeMany = db.collection<Model>(c).deleteMany;
+      return {
+        create,
+        update,
+        one,
+        oneOrThrow,
+        list,
+        remove,
+        removeMany,
+      };
     };
-    const update = (by: any, props: PartialNull<Models[T]>) => {
-      return db.collection(c).updateOne(
-        by,
-        {
-          $set: props,
-        },
-        {
-          upsert: false,
-        },
-      );
-    };
-    const one = (filter: any) => {
-      return db.collection(c).findOne(filter) as Promise<Models[T] | undefined>;
-    };
-    const oneOrThrow = async (filter: any) => {
-      const o = await db.collection(c).findOne(filter);
-      if (!o) {
-        throw new Error(\`Cannot find \${c} object for that params\`);
-      }
-      return o as Promise<Models[T]>;
-    };
-    const list = (filter: any = {}) => {
-      return db
-        .collection(c)
-        .find(filter)
-        .toArray() as Promise<Models[T][]>;
-    };
-    const remove = (filter: any) => {
-      return db.collection(c).deleteOne(filter);
-    };
-    const removeMany = (filter: any) => {
-      return db.collection(c).deleteMany(filter);
-    };
-    return {
-      create,
-      update,
-      one,
-      oneOrThrow,
-      list,
-      remove,
-      removeMany,
-    };
-  };
-  `,
+    `,
   };
 };
