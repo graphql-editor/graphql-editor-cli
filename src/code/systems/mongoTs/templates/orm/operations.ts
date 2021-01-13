e79b58a2024e458023891bc065e8dbbfc568996d
aexol <aexol@aexol.com>
2021-01-13 23:41:12 +0100

    orm template

--- src/code/systems/mongoTs/templates/orm/operations.ts
+++ src/code/systems/mongoTs/templates/orm/operations.ts
@@ -1,50 +1,49 @@
 export const operations = () => {
   return {
     ts: `import {
-      Db,
-      FilterQuery,
-      ObjectId,
-      OptionalId,
-      CollectionInsertOneOptions,
-      UpdateQuery,
-      UpdateOneOptions,
-    } from 'mongodb';
-    import * as collections from './collections';
-    import { Models } from './models';
-    
-    export const Orm = <T extends keyof Models>(db: Db, collection: T) => {
-      type Model = Models[T] & { _id: ObjectId };
-      const c = collections[collection];
-      const col = db.collection<Model>(c);
-      const create = (docs: OptionalId<Model>, options?: CollectionInsertOneOptions) =>
-        db.collection<Model>(c).insertOne(docs, options);
-      const update = (
-        filter: FilterQuery<Model>,
-        update: UpdateQuery<Model> | Partial<Model>,
-        options?: UpdateOneOptions,
-      ) => db.collection<Model>(c).updateOne(filter, update, options);
-      const one = (filter: FilterQuery<Model>) => db.collection<Model>(c).findOne(filter);
-      const oneOrThrow = async (filter: FilterQuery<Model>) => {
-        const o = await db.collection<Model>(c).findOne(filter);
-        if (!o) {
-          throw new Error(\`Cannot find \${c} object for that params\`);
-        }
-        return o;
-      };
-      const list = (filter: FilterQuery<Model> = {}) => db.collection<Model>(c).find(filter).toArray();
-      const remove = (filter: FilterQuery<Model> = {}) => db.collection<Model>(c).deleteOne(filter);
-      const removeMany = (filter: FilterQuery<Model> = {}) => db.collection<Model>(c).deleteMany(filter);
-      return {
-        col,
-        create,
-        update,
-        one,
-        oneOrThrow,
-        list,
-        remove,
-        removeMany,
-      };
-    };
-    `,
+  Db,
+  FilterQuery,
+  ObjectId,
+  OptionalId,
+  CollectionInsertOneOptions,
+  UpdateQuery,
+  UpdateOneOptions,
+} from 'mongodb';
+import * as collections from './collections';
+import { Models } from './models';
+
+export const Orm = <T extends keyof Models>(db: Db, collection: T) => {
+  type Model = Omit<Models[T], 'id' | '_id'> & { _id: ObjectId };
+  const c = collections[collection];
+  const col = db.collection<Model>(c);
+  const create = (docs: OptionalId<Model>, options?: CollectionInsertOneOptions) =>
+    db.collection<Model>(c).insertOne(docs, options);
+  const update = (
+    filter: FilterQuery<Model>,
+    update: UpdateQuery<Model> | Partial<Model>,
+    options?: UpdateOneOptions,
+  ) => db.collection<Model>(c).updateOne(filter, update, options);
+  const one = (filter: FilterQuery<Model>) => db.collection<Model>(c).findOne(filter);
+  const oneOrThrow = async (filter: FilterQuery<Model>) => {
+    const o = await db.collection<Model>(c).findOne(filter);
+    if (!o) {
+      throw new Error(\`Cannot find \${c} object for that params\`);
+    }
+    return o;
+  };
+  const list = (filter: FilterQuery<Model> = {}) => db.collection<Model>(c).find(filter).toArray();
+  const remove = (filter: FilterQuery<Model> = {}) => db.collection<Model>(c).deleteOne(filter);
+  const removeMany = (filter: FilterQuery<Model> = {}) => db.collection<Model>(c).deleteMany(filter);
+  return {
+    col,
+    create,
+    update,
+    one,
+    oneOrThrow,
+    list,
+    remove,
+    removeMany,
+  };
+};`,
   };
 };
