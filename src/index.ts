af6df880f66cea0cf3eda6f2f7a5609f9d1c98d3
aexol <aexol@aexol.com>
2021-04-28 14:12:52 +0200

    move options to its correct place

--- src/index.ts
+++ src/index.ts
@@ -38,79 +38,115 @@ welcome().then(() => {
     .usage('Usage: $0 <command> [options]')
     .help('h')
     .alias('h', 'help')
-    .command('init', 'Create editor project config inside current working directory.', async (yargs) => {
-      await Auth.login().then(Config.setTokenOptions);
-      await initConfiguration(yargs.argv as Pick<ConfigurationOptions, 'project' | 'namespace' | 'version'>);
-    })
-    .options(
-      confOptions({
-        ...projectOptions,
-      }),
+    .command(
+      'init',
+      'Create editor project config inside current working directory.',
+      async (yargs) => {
+        yargs.options(
+          confOptions({
+            ...projectOptions,
+          }),
+        );
+      },
+      async (argv) => {
+        await Auth.login().then(Config.setTokenOptions);
+        await initConfiguration(argv as Pick<ConfigurationOptions, 'project' | 'namespace' | 'version'>);
+      },
     )
-    .command('schema', 'Generate GraphQL schema from project at given path', async (yargs) => {
-      await Auth.login().then(Config.setTokenOptions);
-      await CommandSchema(yargs.argv as Pick<ConfigurationOptions, 'project' | 'namespace' | 'version' | 'schemaDir'>);
-    })
-    .options(
-      confOptions({
-        ...projectOptions,
-        schemaDir: {
-          describe: 'Path to created schema containing its name and extension',
-          type: 'string',
-        },
-      }),
+    .command(
+      'schema',
+      'Generate GraphQL schema from project at given path',
+      async (yargs) => {
+        yargs.options(
+          confOptions({
+            ...projectOptions,
+            schemaDir: {
+              describe: 'Path to created schema containing its name and extension',
+              type: 'string',
+            },
+          }),
+        );
+      },
+      async (argv) => {
+        await Auth.login().then(Config.setTokenOptions);
+        await CommandSchema(argv as Pick<ConfigurationOptions, 'project' | 'namespace' | 'version' | 'schemaDir'>);
+      },
     )
-    .command('typings', 'Generate GraphQL typings for TypeScript or Javascript', async (yargs) => {
-      await Auth.login().then(Config.setTokenOptions);
-      await CommandTypings(
-        yargs.argv as Pick<
-          ConfigurationOptions,
-          'project' | 'namespace' | 'version' | 'typingsDir' | 'typingsEnv' | 'typingsGen' | 'typingsHost'
-        >,
-      );
-    })
-    .options(
-      confOptions({
-        ...projectOptions,
-        typingsGen: {
-          describe: 'Generation language',
-          choices: ['Javascript', 'TypeScript'],
-        },
-        typingsHost: {
-          describe: 'GraphQL Server address',
-          type: 'string',
-        },
-        typingsDir: {
-          describe: 'Path where to create generated files',
-          type: 'string',
-        },
-        typingsEnv: {
-          describe: 'Generation Environment',
-          choices: ['browser', 'node'],
-        },
-      }),
+    .command(
+      'typings',
+      'Generate GraphQL typings for TypeScript or Javascript',
+      async (yargs) => {
+        yargs.options(
+          confOptions({
+            ...projectOptions,
+            typingsGen: {
+              describe: 'Generation language',
+              choices: ['Javascript', 'TypeScript'],
+            },
+            typingsHost: {
+              describe: 'GraphQL Server address',
+              type: 'string',
+            },
+            typingsDir: {
+              describe: 'Path where to create generated files',
+              type: 'string',
+            },
+            typingsEnv: {
+              describe: 'Generation Environment',
+              choices: ['browser', 'node'],
+            },
+          }),
+        );
+      },
+      async (argv) => {
+        await Auth.login().then(Config.setTokenOptions);
+        await CommandTypings(
+          argv as Pick<
+            ConfigurationOptions,
+            'project' | 'namespace' | 'version' | 'typingsDir' | 'typingsEnv' | 'typingsGen' | 'typingsHost'
+          >,
+        );
+      },
     )
-    .command('bootstrap', 'Bootstrap a new frontend or backend project', async (yargs) => {
-      await CommandBootstrap(yargs.argv as Pick<ConfigurationOptions, 'project' | 'namespace' | 'version' | 'system'>);
-    })
-    .options(
-      confOptions({
-        ...projectOptions,
-        system: {
-          type: 'string',
-          choices: ['backend', 'frontend'],
-          describe: 'Choose the type of app you want to bootstrap',
-        },
-      }),
+    .command(
+      'bootstrap',
+      'Bootstrap a new frontend or backend project',
+      async (yargs) => {
+        yargs.options(
+          confOptions({
+            ...projectOptions,
+            system: {
+              type: 'string',
+              choices: ['backend', 'frontend'],
+              describe: 'Choose the type of app you want to bootstrap',
+            },
+          }),
+        );
+      },
+      async (argv) => {
+        await CommandBootstrap(argv as Pick<ConfigurationOptions, 'project' | 'namespace' | 'version' | 'system'>);
+      },
     )
-    .command('resolver', 'Create resolver for your backend project', async (yargs) => {
-      await CommandResolver(yargs.argv as Pick<ConfigurationOptions, 'project' | 'namespace' | 'version'>);
-    })
-    .options(confOptions({ ...projectOptions }))
-    .command('models', 'Generate model files for your backend project', async (yargs) => {
-      await CommandModels(yargs.argv as Pick<ConfigurationOptions, 'project' | 'namespace' | 'version'>);
-    })
-    .options(confOptions({ ...projectOptions }))
+    .command(
+      'resolver',
+      'Create resolver for your backend project',
+      async (yargs) => {
+        yargs.options(confOptions({ ...projectOptions }));
+      },
+      async (argv) => {
+        await CommandResolver(argv as Pick<ConfigurationOptions, 'project' | 'namespace' | 'version'>);
+      },
+    )
+    .command(
+      'models',
+      'Generate model files for your backend project',
+      async (yargs) => {
+        yargs.options(confOptions({ ...projectOptions }));
+      },
+      async (argv) => {
+        await CommandModels(argv as Pick<ConfigurationOptions, 'project' | 'namespace' | 'version'>);
+      },
+    )
     .showHelpOnFail(true)
     .demandCommand()
     .epilog('Bye!').argv;
