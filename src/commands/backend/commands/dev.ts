aa651b1086988284d0b5192d23b38cf36210e615
Kamil Matysiewicz <kamil@aexol.com>
2022-03-01 12:24:52 +0100

    Debounce success

--- src/commands/backend/commands/dev.ts
+++ src/commands/backend/commands/dev.ts
@@ -1,15 +1,36 @@
 import { logger } from '@/common/log/index.js';
 import Tscwatch from 'tsc-watch/client.js';
 import { stucco } from 'stucco-js/lib/stucco/run.js';
-import { ChildProcessByStdio, spawn } from 'child_process';
-import { SIGINT, SIGTERM } from 'constants';
+import { ChildProcess, spawn } from 'child_process';
+import { SIGINT } from 'constants';
 
 const client = new Tscwatch();
+
+const terminate = async (ch?: ChildProcess): Promise<void | number> => {
+  if (!ch) return;
+  const ret = new Promise<number>((resolve) => ch.on('close', (code: number) => resolve(code)));
+  ch.kill(SIGINT);
+  return ret;
+}
+
+const spawnPromise = async (cmd: string, args: string[]): Promise<ChildProcess> => {
+  const child = spawn(cmd, args, {
+    stdio: [process.stdin, process.stdout, process.stderr],
+  });
+  return new Promise<ChildProcess>((resolve, reject) => {
+    child.on('error', reject);
+    child.on('spawn', () => {
+      child.off('error', reject);
+      resolve(child);
+    });
+  });
+}
 
 export const CommandDev = async ({ namespace, project }: { namespace?: string; project?: string }) => {
   const bin = await stucco();
   const args = ['local', 'start'];
-  let child: ChildProcessByStdio<null, null, null> | undefined;
+  let child: ChildProcess | undefined;
+  let delay: NodeJS.Timeout | undefined;
   process.on('SIGTERM', () => {
     child?.kill(SIGINT);
   });
@@ -17,11 +38,17 @@ export const CommandDev = async ({ namespace, project }: { namespace?: string; p
     child?.kill(SIGINT);
   });
   client.on('success', () => {
-    child?.kill(SIGINT);
-    child = spawn(bin.path(), args, {
-      stdio: [process.stdin, process.stdout, process.stderr],
-    });
-    logger('tsc success', 'success');
+    if (delay) clearTimeout(delay);
+    delay = setTimeout(async () => {
+      const ch = child;
+      child = undefined;
+      const code = await terminate(ch);
+      logger(`${code}`, 'info');
+      if (code) logger( `child terminated with non 0 status`, 'info');
+      if (child) return;
+      child = await spawnPromise(bin.path(), args);
+      logger('tsc success', 'success');
+    }, 1000);
   });
   client.on('compile_errors', () => {
     logger('tsc error', 'error');
