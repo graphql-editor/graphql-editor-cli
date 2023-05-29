fc2acd41d6539a2a6e967c28219d5fbb055f46f6
TomArt <tomasz.artecki@aexol.com>
2023-05-29 11:56:08 +0200

    refactoring pushStuccoJson.ts

--- packages/cli/src/commands/cloud/pushStuccoJson.ts
+++ packages/cli/src/commands/cloud/pushStuccoJson.ts
@@ -4,13 +4,10 @@ import { Editor } from '@/Editor.js';
 import { CLOUD_FOLDERS } from '@/gshared/constants/index.js';
 import path from 'path';
 import fs from 'fs';
-import mime from 'mime';
 
 export const pushStuccoJson = async ({ namespace, project }: { namespace?: string; project?: string }) => {
     const resolve = await Config.configure({ namespace, project }, ['namespace', 'project']);
     const p = await Editor.fetchProject({ accountName: resolve.namespace, projectName: resolve.project });
-    // const stuccoFile = fs.readFileSync('stucco.json').toString('utf-8');
-    const stuccoFile = 'stucco.json';
     if (!p.team?.id) {
         throw new Error('No team for this project. Invalid project');
     }
@@ -18,8 +15,8 @@ export const pushStuccoJson = async ({ namespace, project }: { namespace?: strin
     await Editor.saveFilesToCloud(
         p.id,
         [{
-            name: path.join(CLOUD_FOLDERS.microserviceJs, stuccoFile),
-            content: fs.readFileSync(stuccoFile),
+            name: path.join(CLOUD_FOLDERS.microserviceJs, 'stucco.json'),
+            content: fs.readFileSync('stucco.json'),
             type: 'text/plain',
         }]
     );
