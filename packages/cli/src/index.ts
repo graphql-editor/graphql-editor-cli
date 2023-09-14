66c43fb1cf8af6c569f109870e8e2f2fef7bb976
TakSeBiegam <kurylo.ao@gmail.com>
2023-09-14 12:00:42 +0200

    chore: removed unused import and bump version

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -21,7 +21,6 @@ import { confOptions, projectOptions } from '@/common/promptOptions.js';
 import { CommandPrune } from '@/commands/common/prune.js';
 import { CommandInspect } from '@/commands/common/inspect.js';
 import { pushStuccoJson } from '@/commands/cloud/pushStuccoJson.js';
-import { cwd } from 'process';
 
 welcome().then(() => {
   new Configuration();
